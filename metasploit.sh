ead4deddbc803a0f2cd94b8550f2dc9e158fa483
root <root@localhost.localdomain>
2018-09-01 13:44:43 -0300

    Version 0.0.1

---
+++ metasploit.sh
@@ -0,0 +1,5 @@
+####################################################
+git clone https://github.com/B1t5/.metasploit.git  #
+cd .metasploit					   #
+./metasploit.sh					   #
+####################################################
